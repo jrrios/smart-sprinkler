789203302edd6ee81361e6d9771056c04de0cf0c
Richard Huynh <Richard Huynh>
2016-06-04 18:27:45 -0500

    progress of ui; pushing to start merges

--- src/sprinkler_dashboard/views.py
+++ src/sprinkler_dashboard/views.py
@@ -3,8 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 def dashboard(request):
 	title = "Smart Sprinkler System"
+	status = True
 
 	context = {
 		'title': title,
+		'status': status,
 	}
+
 	return render (request, 'dashboard.html', context)
