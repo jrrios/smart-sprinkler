806db8a5fc057a8fbae4857321fa23b5c2f72f4e
Seth Taplin <sethtaplin@gmail.com>
2016-06-05 09:58:28 -0500

    Added min value validators

--- src/sprinkler_dashboard/forms.py
+++ src/sprinkler_dashboard/forms.py
@@ -1,12 +1,13 @@
 from django import forms
 from django.core.validators import MaxValueValidator
+from django.core.validators import MinValueValidator
 
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout
 
 class PropertyForm(forms.Form):
-	address_digit = forms.IntegerField(validators=[MaxValueValidator(9)])
-	property_type = forms.IntegerField(validators=[MaxValueValidator(9)])
+	address_digit = forms.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(9)])
+	property_type = forms.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(2)])
 
 	# def __init__(self, *args, **kwargs):
 	# 	super(PropertyForm, self).__init__(*args, **kwargs)
